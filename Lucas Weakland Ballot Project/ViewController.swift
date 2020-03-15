e5966e6608f58e4a934c30a4c2179e9359020dde
Lucas Weakland <lm.weakland@setonhill.edu>
2020-03-15 15:06:19 -0400

    Initial Commit

---
+++ Lucas Weakland Ballot Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lucas Weakland Ballot Project
+//
+//  Created by Lucas Weakland on 3/15/20.
+//  Copyright © 2020 Lucas Weakland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
